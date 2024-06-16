987108cfdca82ef6b086ec4bf1ff80d8da8eb9b2
Kingdavid <kingigbayilola@gmail.com>
2024-06-16 23:52:37 +0100

    initial commit

---
+++ src/index.mjs
@@ -0,0 +1,22 @@
+import express from "express";
+
+
+
+
+// create express app
+const app = express();
+
+
+//  Third party middlewares
+
+
+// custom middlewares
+
+
+
+// route middlewares
+
+
+
+
+// create and setup express server
